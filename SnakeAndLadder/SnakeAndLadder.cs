14ffb1040fed83c53f18888f089a9b13e6c98854
Anisha0906 <anishadas880@gmail.com>
2022-08-01 21:27:55 +0530

    [Anisha]ADD:checks_for_a_Option

--- SnakeAndLadder/SnakeAndLadder.cs
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -12,10 +12,31 @@ namespace SnakeAndLadder
         {
             int StartPoint = 0;
             int RanDice;
+            int ComputerChoice;
+            int Option = 0;
             Console.WriteLine("Game Started! Good Luck ");
             Random Random = new Random(); 
             RanDice = Random.Next(1, 7); 
             Console.WriteLine("player Role Dice number is:" + " " + RanDice);
+            ComputerChoice = Random.Next(0, 3);  
+            Console.WriteLine("Dice value:" + ComputerChoice); 
+            switch (ComputerChoice) 
+            {
+                case 0:                            
+                    Console.WriteLine("No Play");
+                    break;
+                case 1:                            
+                    StartPoint += RanDice;           
+                    Console.WriteLine("Got Ladder:" + StartPoint);
+                    break;
+                case 2:                                  
+                    StartPoint -= RanDice;              
+                    Console.WriteLine("Snake Attack:" + StartPoint);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");  
+                    break;
+            }
         }
     }
 }
