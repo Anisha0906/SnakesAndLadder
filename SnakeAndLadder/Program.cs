e1f7e7dbaea7b425c1c0c8f9dd59c7b55734bed4
Anisha0906 <anishadas880@gmail.com>
2022-08-01 20:45:39 +0530

    [Anisha] Add : UC1 for single player

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Sumulation Program");
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+            snakeAndLadder.StartPlaying();
+            Console.ReadKey();
+        }
+    }
+}
